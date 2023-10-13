a96f1cc83402b772f3e6dc39b4787b14247fe469
Valande <valande@gmail.com>
2023-10-14 00:02:37 +0200

    Qwiklab02 regex

--- osint/util/conn2lab.sh
+++ osint/util/conn2lab.sh
@@ -3,9 +3,9 @@
 # Connect to qwiklabs via ssh, 
 # replacing username, ipaddress and keyfile first 
 
-username="student-00-951e14db22d5"
-ipaddress="35.190.150.63"
-keyfile="~/Descargas/qwiklabs-L78288381.pem"
+username="student-00-d5d35fbcb3ed"
+ipaddress="34.171.93.139"
+keyfile="/home/v4l4nd3/Descargas/qwiklabs-L78420733.pem"
 
 chmod 600 ${keyfile}
 ssh -i ${keyfile} ${username}@${ipaddress}
