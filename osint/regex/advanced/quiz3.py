57ce0bc251f67db9e1190bd0bf2cde4bb5f5400d
Valande <valande@gmail.com>
2023-10-13 21:48:28 +0200

    advanced regex quiz

---
+++ osint/regex/advanced/quiz3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import re
+
+def transform_comments(line_of_code):
+  result = re.sub(r"\#{1,}", "//", line_of_code)
+  return result
+
+print(transform_comments("### Start of program")) 
+# Should be "// Start of program"
+print(transform_comments("  number = 0   ## Initialize the variable")) 
+# Should be "  number = 0   // Initialize the variable"
+print(transform_comments("  number += 1   # Increment the variable")) 
+# Should be "  number += 1   // Increment the variable"
+print(transform_comments("  return(number)")) 
+# Should be "  return(number)"
