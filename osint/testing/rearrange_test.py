88e925531cfd0d93c22c2e6a1e1221683b1a88d5
Valande <valande@gmail.com>
2023-10-19 11:47:47 +0200

    unit testing, edge cases

--- osint/testing/rearrange_test.py
+++ osint/testing/rearrange_test.py
@@ -9,4 +9,9 @@ class TestRearrange(unittest.TestCase):
         expected = "Ada Lovelace"
         self.assertEqual(rearrange_name(testcase), expected)
 
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+        
 unittest.main()
