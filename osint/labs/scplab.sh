9f7e9294f1a6642d98cff1c9840a6acd26e2331d
Valande <valande@gmail.com>
2023-10-19 13:38:51 +0200

    improve qwiklabs conn. helper scripts

--- osint/labs/scplab.sh
+++ osint/labs/scplab.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/bash
 
+# Source oneshot credentials
+. ./shlib/oneshotcred.sh
+
 # Download qwiklabs resources via scp, 
-# replacing username, ipaddress and keyfilename first 
-
-username="student-00-d5d35fbcb3ed"
-ipaddress="35.196.113.103"
-keyfilename="qwiklabs-L79440969.pem"
-
 keyfile="/home/v4l4nd3/Descargas/${keyfilename}"
 chmod 600 ${keyfile}
 scp -i ${keyfile} -r ${username}@${ipaddress}:/home/${username} ./
---
+++ osint/labs/shlib/oneshotcred.sh
@@ -0,0 +1,4 @@
+
+export username="student-00-d5d35fbcb3ed"
+export ipaddress="35.196.113.103"
+export keyfilename="qwiklabs-L79440969.pem"
