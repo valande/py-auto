ffd3dc8d724c31b9b1d53aecb01a5630288284d7
Valande <valande@gmail.com>
2023-10-19 23:42:30 +0200

    Qwiklabs utils for VCS and Git

---
+++ vcs/labs/shlib/oneshotcred.sh
@@ -0,0 +1,8 @@
+
+export username="student-00-abbb6f97e935"
+export ipaddress="34.127.109.243"
+export keyfilename="qwiklabs-L79792510.pem"
+
+
+# upload to qwiklab command:
+# scp -i ~/Descargas/qwiklabs-L79792510.pem *.py student-00-abbb6f97e935@34.127.109.243:/home/student-00-abbb6f97e935/scripts/
